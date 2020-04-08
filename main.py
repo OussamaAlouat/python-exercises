a364a4f0f3c1c962dea6f7a3e0930e43be4b6044
OussamaAlouat <al317257@uji.es>
2020-04-08 22:15:57 +0200

    added exercise 2

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
  #version: 0.1.0
 
 from exercises.exercise1 import exercise1
+from exercises.exercise2 import exercise2
 
 print('Welcome to the python exercises repository')
 print('##########################################')
 exercise1()
+print('##########################################')
+exercise2()
