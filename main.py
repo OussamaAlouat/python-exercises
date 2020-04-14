fa06096d866cb178e44cc61990f06d8a03c8f59a
OussamaAlouat <al317257@uji.es>
2020-04-14 18:37:54 +0200

    Added execution of new exercise

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 
 from exercises.exercise1 import exercise1
 from exercises.exercise2 import exercise2
+from exercises.exercise3 import exercise3
 
-exercises = [exercise1, exercise2]
+exercises = [exercise1, exercise2, exercise3]
 print('Welcome to the python exercises repository')
 print('##########################################')
 
